826c26474a787dc45b74f91e001f6970866e577c
braden <leachbr787@northwested.org>
2025-01-15 13:37:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+#Braden Leach
+#January 15 2024
+#Dictionaries Starter
 
+
+
+
+#--Create and Add K.V.P to Dict--#
+
+my_dict = {'name':'Braden','age': 17,'city':'Buckley'}
+
+
+print(my_dict['name'])
+
+
+#--Updating K.V.P in a Dict--#
+
+my_dict = {'name':'Braden','age': 17}
+my_dict['age'] = 25
+
+
+print(my_dict)
