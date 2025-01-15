2abc9f587acf6f96ff6efbed256ee8f83a3985da
braden <leachbr787@northwested.org>
2025-01-15 13:37:52 -0500

    Create contacts.py

---
+++ contacts.py
@@ -0,0 +1,19 @@
+#Braden Leach
+#January 15 2024
+#Contacts
+
+
+#--Practical Application--#
+
+contacts = {
+        'Braden': '231-774-2901',
+        'Cameron': '987-654-3210',
+        'Alayna':'266-223-8683',
+        'David':'247-383-2199',
+        'Pablo':'402-625-4474',
+        'Kristian':'555-287-6301',
+        'Liz':'614-147-3738',
+}
+contacts['Liz'] = '257-519-2676'
+
+print(contacts)
